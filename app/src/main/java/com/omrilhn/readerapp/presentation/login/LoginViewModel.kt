77ebceaa788a3633858dee4004ac40a0fac633ee
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-12-09 13:59:38 +0300

    Update LoginVM using State for e-mail

--- app/src/main/java/com/omrilhn/readerapp/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/omrilhn/readerapp/presentation/login/LoginViewModel.kt
@@ -6,18 +6,22 @@ import com.omrilhn.readerapp.data.repository.LoginRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(private val loginRepository: LoginRepository) : ViewModel() {
-    private val _emailText = MutableStateFlow<String>("")
-    val emailTextState: StateFlow<String> get() = _emailText
+    private val _emailTextState = MutableStateFlow(StandardTextFieldState())
+    val emailTextState: StateFlow<StandardTextFieldState> = _emailTextState
 
     private val _passwordText = MutableStateFlow<String>("")
     val passwordText : StateFlow<String> get() = _passwordText
 
     fun setEmailText(email:String){
-        _emailText.value = email
+        //Setting email text for MutableStateFlow using State
+        _emailTextState.update {currentState-> //Also use StateFlow's update method
+            currentState.copy(text = email)
+        }
     }
     fun setPasswordText(password:String){
         _passwordText.value = password
