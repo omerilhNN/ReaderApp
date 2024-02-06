2c1274ef47603b5dcb7987b31384022857cbdcff
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2024-02-06 23:16:26 +0300

    Center elements in ShowBookDetailsScreen

--- app/src/main/java/com/omrilhn/readerapp/presentation/details/ShowBookDetails.kt
+++ app/src/main/java/com/omrilhn/readerapp/presentation/details/ShowBookDetails.kt
@@ -2,15 +2,20 @@ package com.omrilhn.readerapp.presentation.details
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -27,26 +32,44 @@ fun ShowBookDetails(bookInfo: Resource<Item>,
     val bookData = bookInfo.data?.volumeInfo
     val googleBookId = bookInfo.data?.id
 
-    Card(modifier = Modifier.padding(20.dp),
-        shape = RectangleShape,
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-        border = BorderStroke(width = 2.dp, color = Color.DarkGray)
-    ){
 
-        Image(painter = rememberAsyncImagePainter(model = bookData!!.imageLinks.thumbnail),
-            contentDescription ="Book Images",
-            modifier = Modifier.width(150.dp)
-                .height(200.dp)
-                .padding(2.dp))
+
+    LazyColumn(modifier = Modifier.fillMaxSize().padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        item(){
+            Card(modifier = Modifier.padding(2.dp),
+                shape = RectangleShape,
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                border = BorderStroke(width = 2.dp, color = Color.DarkGray)
+            ){
+
+                Image(painter = rememberAsyncImagePainter(model = bookData!!.imageLinks.thumbnail),
+                    contentDescription ="Book Images",
+                    alignment = Alignment.Center,
+                    modifier = Modifier
+                        .width(150.dp)
+                        .height(200.dp)
+                        .padding(2.dp))
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally){
+                Text(text = bookData?.title.toString(),
+                    style = MaterialTheme.typography.h6,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 20)
+                Text(text = "Authors: ${bookData?.authors.toString()}")
+                Text(text = "Page count: ${bookData?.pageCount.toString()}")
+                Text(text = "Categories: ${bookData?.categories.toString()}",
+                    style = MaterialTheme.typography.subtitle1)
+                Text(text = "Published: ${bookData?.publishedDate.toString()}",
+                    style = MaterialTheme.typography.subtitle1)
+                Spacer(modifier = Modifier.padding(5.dp))
+            }
+
+
+        }
+
+
+
+
     }
-    Text(text = bookData?.title.toString(),
-        style = MaterialTheme.typography.h6,
-        overflow = TextOverflow.Ellipsis,
-        maxLines = 20)
-    Text(text = "Authors: ${bookData?.authors.toString()}")
-    Text(text = "Page count: ${bookData?.pageCount.toString()}")
-    Text(text = "Categories: ${bookData?.categories.toString()}",
-        style = MaterialTheme.typography.subtitle1)
-    Text(text = "Published: ${bookData?.publishedDate.toString()}",
-        style = MaterialTheme.typography.subtitle1)
 }
